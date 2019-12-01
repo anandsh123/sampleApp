26302be894763743cbf464aa6cb51fd871e495c3
anandsh123 <anand.sh08@gmail.com>
2019-12-02 02:09:26 +0530

    First example

---
+++ src/app/core/services/api-interceptor.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, Injector } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpHandler, HttpRequest, HttpEvent, HttpResponse, HttpErrorResponse, HttpInterceptor } from "@angular/common/http";
+
+import { environment } from '../../../environments/environment';
+import { AuthenticationService } from './authentication.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiInterceptorService {
+
+  constructor(private injector: Injector, private router: Router) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request);
+  }
+}
